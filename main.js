95b177b058e9627da2b17e0f148de6aa919a9603
Arigam1 <yebkobvsebolshe@gmail.com>
2021-02-02 01:51:27 +0400

    helper, serverinfo+ fix bugs

--- main.js
+++ main.js
@@ -4,26 +4,61 @@ const client = new Discord.Client()
 
 const config = require('./config.json')
 const command = require('./command')
+const privateMessage = require('./private-message')
 
 
 client.on('ready', () => {
     console.log('ehuuu')
 
-    command(client, ['arigami','zaver'], (message) => {
-        message.channel.send('King')
+    // ОТКЛИК НА МОЙ НИКНЕЙМ...................................................
+    command(client, ['arigami'], (message) => {
+        message.channel.send('Junior React developer')
     }),
-    command(client, ['markus','daun'], (message) => {
-        message.channel.send('sudba')
-    }),    
 
-    command(client, 'servers', (message) => {
-        client.guilds.cache.forEach((guild) =>{
-            message.channel.send (
-                `${guild.name} всего ${guild.memberCount} ущерба`
+    // КОЛИЧЕСТВО ЛЮДЕЙ НА СЕРВЕРЕ............................................
+    command(client, 'serverinfo', (message) => {
+        const { guild } = message
+
+        const {name, region, memberCount, owner, afkTimeout} = guild
+        const icon = guild.iconURL()
+
+        const embed = new Discord.MessageEmbed()
+            .setColor('#ff0000')
+            .setTitle(`Server info for "${name}"`)
+            .setThumbnail(icon)
+            .addFields(
+                {
+                    name: 'Owner',
+                    value: owner,
+                },
+                { 
+                    name: '\u200B', 
+                    value: '\u200B' 
+                },
+                {
+                    name: 'Region',
+                    value: region,
+                    inline: true,
+                },
+                {
+                    name: 'Members',
+                    value: memberCount,
+                    inline: true,
+                },
+                {
+                    name: 'AFK Timeout',
+                    value: afkTimeout / 60,
+                    inline: true,
+                },
             )
-        })
+            .setImage('https://cutewallpaper.org/21/hacker-logos/Hacker-Logo-Vectors-Photos-and-PSD-files-Free-Download.jpg')
+            .setTimestamp()
+            .setFooter('Copyright by Arigami ©', 'https://i1.wp.com/www.youngpioneertours.com/wp-content/uploads/2020/03/russian-flag-russian-flag-russia-flag-of-russia.jpg?fit=1332%2C850&ssl=1');
+
+        message.channel.send(embed)
     })
 
+    // УДАЛЕНИЕ ВСЕГО ЧАТА ЗА ЛАСТ 14 ДНЕЙ...................................
     command(client, ['уничтожение'], (message) => {
         if (message.member.hasPermission('ADMINISTRATOR')) {
             message.channel.messages.fetch().then((results) => {
@@ -31,7 +66,7 @@ client.on('ready', () => {
             })
         }
     })
-
+    // СТАТУС БОТА...........................................................
     command(client, 'status', (message) =>{
         const content = message.content.replace('!status ','')
 
@@ -41,6 +76,31 @@ client.on('ready', () => {
                 type:0,
             },
         })
+    })
+
+    // РАБОТА С ПРИВАТНЫМИ СООБЩЕНИЯМИ.......................................
+    privateMessage(client, '!freegift','ХачТрюхач hacked your computer. Contact the admin for help (Arigami)')
+
+
+    // ПОМОЩНИК.............................................................
+    command(client, 'help',(message)=>{
+        message.channel.send(`
+These are my supported commands:
+
+**!help** -Display the help menu
+**!serverinfo** - Information about this server
+**!freegift** - Free discord $
+`)
+
+        const {prefix} = config
+
+        client.user.setPresence({
+            activity: {
+                name: `"${prefix}help" for help`,
+            },
+        })
+
+
     })
 })
 
